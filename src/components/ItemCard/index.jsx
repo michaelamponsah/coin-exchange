54b957c24bce319aad4edad81354ccd4c2fa43b9
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-02-08 15:12:55 +0000

    Add styling to card component

--- src/components/ItemCard/index.jsx
+++ src/components/ItemCard/index.jsx
@@ -1,17 +1,34 @@
 import React from 'react';
+import { PropTypes } from 'prop-types';
+import { FiArrowRightCircle } from 'react-icons/fi';
+import './style.css';
 
-const ItemCard = () => (
+const ItemCard = ({ image, name, rank }) => (
   <div className="card-wrapper">
     <div className="card-header">
-      Icon
+      <FiArrowRightCircle className="icon" />
     </div>
     <div className="card-body">
-      <img src="" alt="" />
+      <img src={image} alt={name} />
     </div>
-    <div className="Card-footer">
-      <p>Item name</p>
+    <div className="card-footer">
+      <p>{name}</p>
+      <p>
+        Rating:
+        {rank}
+      </p>
     </div>
   </div>
 );
+
+ItemCard.propTypes = {
+  image: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  rank: PropTypes.number,
+};
+
+ItemCard.defaultProps = {
+  rank: 0,
+};
 
 export default ItemCard;
